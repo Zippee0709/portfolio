38e8bcac3ffa4dbdcaeba7ce60b7d6df61e770b4
zhiwen.wang <zhiwen.wang@epitech.eu>
2023-10-28 06:19:07 +0200

    FIX image background color for better constrat on light mode

--- src/components/app/sections/about-me.tsx
+++ src/components/app/sections/about-me.tsx
@@ -17,33 +17,39 @@ const AboutMe = () => {
               <p className="text-5xl">3</p>
               <p className="text-lg">ans</p>
             </div>
-            <p className="text-sm text-gray-400">d&apos;expériences</p>
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              d&apos;expériences
+            </p>
           </Card>
           <Card>
             <div className="flex items-start gap-1 font-medium">
               <p className="text-5xl">5</p>
               <p className="text-lg">+</p>
             </div>
-            <p className="text-sm text-gray-400">clients statisfaits</p>
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              clients statisfaits
+            </p>
           </Card>
           <Card>
             <div className="flex items-start gap-1 font-medium">
               <p className="text-5xl">10</p>
               <p className="text-lg">+</p>
             </div>
-            <p className="text-sm text-gray-400">projets participés</p>
+            <p className="text-sm text-gray-600 dark:text-gray-400">
+              projets participés
+            </p>
           </Card>
           <Card>
             <div className="flex items-end gap-1 font-medium">
               <p className="text-5xl">100</p>
               <p className="text-lg">%</p>
             </div>
-            <p className="text-sm text-gray-400">passion</p>
+            <p className="text-sm text-gray-600 dark:text-gray-400">passion</p>
           </Card>
         </div>
         <Card className="items-start gap-2 p-6">
           <p className="text-2xl">Coder avec passion.</p>
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-600 dark:text-gray-400">
             Passionné par le code depuis mon admission en école
             d&apos;ingénieurs, je ne cesse de me perfectionner à travers de
             divers projets.
@@ -55,11 +61,11 @@ const AboutMe = () => {
         </Card>
         <Card className="items-start gap-2 pl-6 pt-6">
           <p className="pr-6 text-2xl">Un code propre et efficace.</p>
-          <p className="pr-6 text-sm text-gray-400">
+          <p className="pr-6 text-sm text-gray-600 dark:text-gray-400">
             Une attention particulière est portée sur la qualité et la
             lisibilité du code.
           </p>
-          <div className="bg-gradient-card relative h-36 w-full rounded-tl-xl">
+          <div className="relative h-36 w-full rounded-tl-xl rounded-br-xl bg-gradient-to-b from-slate-900 to-slate-900">
             <Image
               src="/images/code.png"
               alt="Screenshot showing HTML semntic code"
@@ -70,13 +76,13 @@ const AboutMe = () => {
         </Card>
         <Card className="items-start gap-2 pl-6 pt-6">
           <p className="pr-6 text-2xl">Développement responsive.</p>
-          <p className="pr-6 text-sm text-gray-400">
+          <p className="pr-6 text-sm text-gray-600 dark:text-gray-400">
             Avec une approche &quot;mobile-first&quot;, un standard de nos
             jours.
           </p>
-          <div className="relative h-60 w-full rounded-xl">
+          <div className="relative h-60 w-full rounded-tl-xl rounded-br-xl bg-gradient-to-b from-slate-900 to-slate-900">
             <Image
-              className="rounded-xl"
+              className="rounded-br-xl"
               src="/images/responsive.webp"
               alt="Screenshot showing the responsiveness of an website"
               fill
